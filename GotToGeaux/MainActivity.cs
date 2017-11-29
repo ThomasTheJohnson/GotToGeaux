20ed9c509aed0d43d3f7b9530314851f97f2b472
Thomas Johnson <tjohnson061944@gmail.com>
2017-11-28 21:44:59 -0600

    Add project files.

---
+++ GotToGeaux/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace GotToGeaux
+{
+    [Activity(Label = "GotToGeaux", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private Button SignUpButton;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            SignUpButton = FindViewById<Button>(Resource.Id.signUpButton);
+            SignUpButton.Click += SignUpButton_Click;
+        }
+
+        private void SignUpButton_Click(object sender, System.EventArgs e)
+        {
+            //Pulls up the sign up screen
+            FragmentTransaction transaction = FragmentManager.BeginTransaction();
+            SignUpDialog signUpDialog = new SignUpDialog();
+            signUpDialog.Show(transaction, "dialog fragment");
+        }
+    }
+}
+
---
+++ GotToGeaux/SignUpDialog.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace GotToGeaux
+{
+    public class OnSignUpEventArgs : EventArgs
+    {
+        private string FirstName;
+        private string Email;
+        private string Password;
+
+        public string getFirstName;
+  
+
+
+    }
+    class SignUpDialog : DialogFragment
+    {
+        //global variables of the activities
+        private EditText InputFirstName;
+        private EditText InputEmail;
+        private EditText InputPassword;
+        private Button SignUpButton;
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            base.OnCreateView(inflater, container, savedInstanceState);
+
+            var view = inflater.Inflate(Resource.Layout.SignUpDialog, container, false);
+
+            //These variables allow us to grab events and data from the buttons themselves.
+            InputFirstName = view.FindViewById<EditText>(Resource.Id.signUpFirstNameText);
+            InputEmail = view.FindViewById<EditText>(Resource.Id.signUpEmailText);
+            InputPassword = view.FindViewById<EditText>(Resource.Id.signUpPasswordText);
+            SignUpButton = view.FindViewById<Button>(Resource.Id.signUpButton);
+
+            //SignUpButton.Click += MSignUpButton_Click;
+            return view;
+        }
+
+        private void MSignUpButton_Click(object sender, EventArgs e)
+        {
+            //This is the handler for the user clicking the sign up button
+            
+        }
+
+        public override void OnActivityCreated(Bundle savedInstanceState)
+        {
+            //Gets rid of the title bar
+            Dialog.Window.RequestFeature(WindowFeatures.NoTitle);
+            base.OnActivityCreated(savedInstanceState);
+            //sets the animation of the window
+            Dialog.Window.Attributes.WindowAnimations = Resource.Style.signup_animation;
+        }
+
+    }
+}
